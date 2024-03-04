a6a5b490b3a2e553b8f4a7fccb90e814d9ba7e68
Reece Harris <reeceharris@email.com>
2024-03-04 14:01:56 +0000

    Add database and error handeling

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@
 
 import path from 'path';
 import dotenv from 'dotenv';
-import express, { Request, Response } from 'express';
+
 import compression from 'compression';
 import staticServe from 'serve-static';
+import sequelize from './database';
 import { create } from 'express-handlebars';
-import appRouter from './routes/app';
+import express, { Request, Response } from 'express';
 
 dotenv.config()
 const app = express();
@@ -24,12 +25,42 @@ app.use(compression({ threshold: 150, level: 9 }));
 app.use('/assets/', staticServe(path.join(__dirname, 'public')));
 
 /* Routes */
+import appRouter from './routes/app';
 app.use('/', appRouter);
 
 /* Error handling */
-app.use((err: Error, _req: Request, res: Response) => {
-    console.error(err);
-    res.status(500).send('Server Error');
+app.use(async (req: Request, res: Response) => {
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+        return res.send('Not found'); // set to your 404 html page
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        return res.json({ error: 'Not found' });
+    }
+
+    // default to plain-text. send()
+    return res.type('txt').send('Not found');
+});
+
+app.use(function(err, req, res, next){
+    res.status(500);
+
+    // respond with html page
+    if (req.accepts('html')) {
+        return res.send('Internal Server Error'); // set to your 500 html page
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        return res.json({ error: 'Internal Server Error' });
+    }
+
+    // default to plain-text. send()
+    return res.type('txt').send('Internal Server Error');
 });
 
 /* Start server */
